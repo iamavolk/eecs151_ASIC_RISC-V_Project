bf9d70311b348ed68078b92960e424a652de2772
alex volkov <avolkov@berkeley.edu>
2024-04-26 22:39:15 -0700

    Changes:   * Cache.v passes asm tests   * Cosmetic change to Memory151.v   * Added Cache States

--- skel/src/Memory151.v
+++ skel/src/Memory151.v
@@ -49,109 +49,108 @@ wire dc_mem_resp_valid;
 wire [(`MEM_DATA_BITS/8)-1:0]  dc_mem_req_mask;
 
 `ifdef no_cache_mem
-no_cache_mem icache (
-  .clk(clk),
-  .reset(reset),
-  .cpu_req_valid(icache_re),
-  .cpu_req_ready(i_stall_n),
-  .cpu_req_addr(icache_addr[31:2]),
-  .cpu_req_data(), // core does not write to icache
-  .cpu_req_write(4'b0), // never write
-  .cpu_resp_valid(),
-  .cpu_resp_data(icache_dout)
-);
-
-no_cache_mem dcache (
-  .clk(clk),
-  .reset(reset),
-  .cpu_req_valid((| dcache_we) || dcache_re),
-  .cpu_req_ready(d_stall_n),
-  .cpu_req_addr(dcache_addr[31:2]),
-  .cpu_req_data(dcache_din),
-  .cpu_req_write(dcache_we),
-  .cpu_resp_valid(),
-  .cpu_resp_data(dcache_dout)
-);
-assign stall =  ~i_stall_n || ~d_stall_n;
-
+    no_cache_mem icache (
+      .clk(clk),
+      .reset(reset),
+      .cpu_req_valid(icache_re),
+      .cpu_req_ready(i_stall_n),
+      .cpu_req_addr(icache_addr[31:2]),
+      .cpu_req_data(), // core does not write to icache
+      .cpu_req_write(4'b0), // never write
+      .cpu_resp_valid(),
+      .cpu_resp_data(icache_dout)
+    );
+    
+    no_cache_mem dcache (
+      .clk(clk),
+      .reset(reset),
+      .cpu_req_valid((| dcache_we) || dcache_re),
+      .cpu_req_ready(d_stall_n),
+      .cpu_req_addr(dcache_addr[31:2]),
+      .cpu_req_data(dcache_din),
+      .cpu_req_write(dcache_we),
+      .cpu_resp_valid(),
+      .cpu_resp_data(dcache_dout)
+    );
+    assign stall =  ~i_stall_n || ~d_stall_n;
 `else
-cache icache (
-  .clk(clk),
-  .reset(reset),
-
-  .cpu_req_valid(icache_re),
-  .cpu_req_ready(i_stall_n),
-  .cpu_req_addr(icache_addr[31:2]),
-
-  .cpu_req_data(), // core does not write to icache
-  .cpu_req_write(4'b0), // never write
-
-  .cpu_resp_valid(),
-  .cpu_resp_data(icache_dout),
-
-  .mem_req_valid(ic_mem_req_valid),
-  .mem_req_ready(ic_mem_req_ready),
-  .mem_req_addr(ic_mem_req_addr),
-  .mem_req_data_valid(),
-  .mem_req_data_bits(),
-  .mem_req_data_mask(),
-  .mem_req_data_ready(),
-  .mem_req_rw(),
-  .mem_resp_valid(ic_mem_resp_valid),
-  .mem_resp_data(mem_resp_data)
-);
-
-cache dcache (
-  .clk(clk),
-  .reset(reset),
-  .cpu_req_valid((| dcache_we) || dcache_re),
-  .cpu_req_ready(d_stall_n),
-  .cpu_req_addr(dcache_addr[31:2]),
-  .cpu_req_data(dcache_din),
-  .cpu_req_write(dcache_we),
-  .cpu_resp_valid(),
-  .cpu_resp_data(dcache_dout),
-  .mem_req_valid(dc_mem_req_valid),
-  .mem_req_ready(dc_mem_req_ready),
-  .mem_req_addr(dc_mem_req_addr),
-  .mem_req_rw(dc_mem_req_rw),
-  .mem_req_data_valid(mem_req_data_valid),
-  .mem_req_data_bits(mem_req_data_bits),
-  .mem_req_data_mask(mem_req_data_mask),
-  .mem_req_data_ready(mem_req_data_ready),
-  .mem_resp_valid(dc_mem_resp_valid),
-  .mem_resp_data(mem_resp_data)
-);
-assign stall =  ~i_stall_n || ~d_stall_n;
-
-//                           ICache 
-//                         /        \
-//   Riscv151 --- Memory151          Arbiter <--> ExtMemModel
-//                         \        /
-//                           DCache 
-
-riscv_arbiter arbiter (
-  .clk(clk),
-  .reset(reset),
-  .ic_mem_req_valid(ic_mem_req_valid),
-  .ic_mem_req_ready(ic_mem_req_ready),
-  .ic_mem_req_addr(ic_mem_req_addr),
-  .ic_mem_resp_valid(ic_mem_resp_valid),
-
-  .dc_mem_req_valid(dc_mem_req_valid),
-  .dc_mem_req_ready(dc_mem_req_ready),
-  .dc_mem_req_rw(dc_mem_req_rw),
-  .dc_mem_req_addr(dc_mem_req_addr),
-  .dc_mem_resp_valid(dc_mem_resp_valid),
-
-  .mem_req_valid(mem_req_valid),
-  .mem_req_ready(mem_req_ready),
-  .mem_req_rw(mem_req_rw),
-  .mem_req_addr(mem_req_addr),
-  .mem_req_tag(mem_req_tag),
-  .mem_resp_valid(mem_resp_valid),
-  .mem_resp_tag(mem_resp_tag)
-);
+    cache icache (
+      .clk(clk),
+      .reset(reset),
+    
+      .cpu_req_valid(icache_re),
+      .cpu_req_ready(i_stall_n),
+      .cpu_req_addr(icache_addr[31:2]),
+    
+      .cpu_req_data(), // core does not write to icache
+      .cpu_req_write(4'b0), // never write
+    
+      .cpu_resp_valid(),
+      .cpu_resp_data(icache_dout),
+    
+      .mem_req_valid(ic_mem_req_valid),
+      .mem_req_ready(ic_mem_req_ready),
+      .mem_req_addr(ic_mem_req_addr),
+      .mem_req_data_valid(),
+      .mem_req_data_bits(),
+      .mem_req_data_mask(),
+      .mem_req_data_ready(),
+      .mem_req_rw(),
+      .mem_resp_valid(ic_mem_resp_valid),
+      .mem_resp_data(mem_resp_data)
+    );
+    
+    cache dcache (
+      .clk(clk),
+      .reset(reset),
+      .cpu_req_valid((| dcache_we) || dcache_re),
+      .cpu_req_ready(d_stall_n),
+      .cpu_req_addr(dcache_addr[31:2]),
+      .cpu_req_data(dcache_din),
+      .cpu_req_write(dcache_we),
+      .cpu_resp_valid(),
+      .cpu_resp_data(dcache_dout),
+      .mem_req_valid(dc_mem_req_valid),
+      .mem_req_ready(dc_mem_req_ready),
+      .mem_req_addr(dc_mem_req_addr),
+      .mem_req_rw(dc_mem_req_rw),
+      .mem_req_data_valid(mem_req_data_valid),
+      .mem_req_data_bits(mem_req_data_bits),
+      .mem_req_data_mask(mem_req_data_mask),
+      .mem_req_data_ready(mem_req_data_ready),
+      .mem_resp_valid(dc_mem_resp_valid),
+      .mem_resp_data(mem_resp_data)
+    );
+    assign stall =  ~i_stall_n || ~d_stall_n;
+    
+    //                           ICache 
+    //                         /        \
+    //   Riscv151 --- Memory151          Arbiter <--> ExtMemModel
+    //                         \        /
+    //                           DCache 
+    
+    riscv_arbiter arbiter (
+      .clk(clk),
+      .reset(reset),
+      .ic_mem_req_valid(ic_mem_req_valid),
+      .ic_mem_req_ready(ic_mem_req_ready),
+      .ic_mem_req_addr(ic_mem_req_addr),
+      .ic_mem_resp_valid(ic_mem_resp_valid),
+    
+      .dc_mem_req_valid(dc_mem_req_valid),
+      .dc_mem_req_ready(dc_mem_req_ready),
+      .dc_mem_req_rw(dc_mem_req_rw),
+      .dc_mem_req_addr(dc_mem_req_addr),
+      .dc_mem_resp_valid(dc_mem_resp_valid),
+    
+      .mem_req_valid(mem_req_valid),
+      .mem_req_ready(mem_req_ready),
+      .mem_req_rw(mem_req_rw),
+      .mem_req_addr(mem_req_addr),
+      .mem_req_tag(mem_req_tag),
+      .mem_resp_valid(mem_resp_valid),
+      .mem_resp_tag(mem_resp_tag)
+    );
 `endif
 
 endmodule
